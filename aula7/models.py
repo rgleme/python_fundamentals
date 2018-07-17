7dc8cbf041323e98aa3f27012d952efd83a6637c
Rodolfo Leme <rodolfo439@gmail.com>
2018-07-16 21:53:44 -0300

    nova aulas

---
+++ aula7/models.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column,Integer,String
+
+engine = create_engine ("sqlite:///banco.db")
+Base = declarative_base()
+
+class Usuarios(Base):
+    __tablename_ = 'usuarios'
+    id = Column(Integer,primary_key=True)
+    nome = Column(String)
+    email = Column(String)
+    senha = Column(String)
+
+if __name__ = 'main'
+    
